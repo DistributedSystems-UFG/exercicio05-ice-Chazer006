439c5ef4c282df4304d72815cfa0a6a3367ee4ff
Pedro Lucas <102874758+Chazer006@users.noreply.github.com>
2026-04-12 22:55:03 -0300

    Atualização

--- client2.py
+++ client2.py
@@ -1,19 +1,17 @@
 import sys, Ice
 import Demo
- 
+
 with Ice.initialize(sys.argv) as communicator:
     base1 = communicator.stringToProxy("SimplePrinter1:tcp -h 127.0.0.1 -p 11000")
-    printer1 = Demo.PrinterPrx.checkedCast(base1)
+    printer1 = Demo.PrinterPrx.checkedCast(base1) [cite: 1]
     
     base_log = communicator.stringToProxy("MeuLogger:tcp -h 127.0.0.1 -p 11000")
     logger = Demo.LoggerPrx.checkedCast(base_log)
 
     if not printer1 or not logger:
-        raise RuntimeError("Erro ao conectar nos servidores")
+        raise RuntimeError("Proxies inválidos")
 
-    res_soma = printer1.add(30, 12)
-    res_sub  = printer1.sub(100, 45)
-    
-    printer1.printString(f"Calculado no servidor: Soma={res_soma}, Sub={res_sub}")
+    soma = printer1.add(10, 5)
+    printer1.printString(f"O resultado da soma eh: {soma}")
 
-    logger.log("O cliente chamou as novas funções matemáticas com sucesso!")
+    logger.log("Cliente finalizou as operacoes com sucesso.")
